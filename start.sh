610510934317f015b7082012ea007c28d67779a7
Oleg Sidenko <oleg@casp.ru>
2018-02-15 10:59:05 +0300

    fix start.sh

--- start.sh
+++ start.sh
@@ -6,8 +6,8 @@ echo "sftpdev:$SFTPDEV_PASSWD" | chpasswd
 
 
 [  -e $WEB_DOCUMENTROOT ] && export WEB_DOCUMENTROOT=/var/www/html
-sed -i "s/_WEB_DOCUMENTROOT_/$WEB_DOCUMENTROOT/a"g /etc/apache2/sites-enabled/apache-default-vhost.conf
-sed -i "s/_WEB_DOCUMENTROOT_/$WEB_DOCUMENTROOT/a"g /etc/nginx/nginx.conf
+sed -i "s,_WEB_DOCUMENTROOT_,$WEB_DOCUMENTROOT,g" /etc/apache2/sites-enabled/apache-default-vhost.conf
+sed -i "s,_WEB_DOCUMENTROOT_,$WEB_DOCUMENTROOT,g" /etc/nginx/nginx.conf
 
 
 if [ "$STATIC_BY_NGINX" == "1" ]; then
