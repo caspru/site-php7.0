dd138a2fce8ad2e0f5d3a443db5c8e3781ba5a2c
Oleg Sidenko <oleg@casp.ru>
2018-02-15 17:29:55 +0300

    fix start.sh

--- start.sh
+++ start.sh
@@ -5,13 +5,13 @@ rm -f /run/crond.pid /run/apache2/apache2.pid
 echo "sftpdev:$SFTPDEV_PASSWD" | chpasswd
 
 
-[  -e $WEB_DOCUMENTROOT ] && export WEB_DOCUMENTROOT=/var/www/html
+[ ! -e $WEB_DOCUMENTROOT ] && export WEB_DOCUMENTROOT=/var/www/html
 sed -i "s,_WEB_DOCUMENTROOT_,$WEB_DOCUMENTROOT,g" /etc/apache2/sites-enabled/apache-default-vhost.conf
 sed -i "s,_WEB_DOCUMENTROOT_,$WEB_DOCUMENTROOT,g" /etc/nginx/nginx.conf
 
 
 if [ "$STATIC_BY_NGINX" == "1" ]; then
-	sed -i '/^#STATIC//g' /etc/nginx/nginx.conf
+	sed -i 's/^#STATIC//g' /etc/nginx/nginx.conf
 fi
 
 
